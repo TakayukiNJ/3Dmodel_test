73cb0184582a4ceb80d9fdb21624b4edbf39b5c6
[TakayukiNJ] <[nj.takayuki@gmail.com]>
2019-10-26 21:46:07 +0900

    アニメーションを2パターンつけて動かした私。

---
+++ Assets/NewBehaviour3DtwstScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviour3DtwstScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Space)){
+			Debug.Log("Space");
+		}
+	}
+}
